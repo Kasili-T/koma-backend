d4cc95c0e21ec070f07a1bbbcb0e68ee4715756b
Kasili-T <godwinkasili146@gmail.com>
2025-11-12 13:40:19 +0300

    Lazy import of ChatConsumer in routing.py
    Changed import method for ChatConsumer in websocket URL patterns.

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from . import consumers
 
+# Import consumers lazily inside list comprehension
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_id>\w+)/$', __import__('chat.consumers').consumers.ChatConsumer.as_asgi()),
 ]
